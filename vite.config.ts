e891e6a7ff8ae2a842a71118d304c083f77a9ed4
vinesagar <38588461+vinesagar@users.noreply.github.com>
2025-08-25 19:34:09 +0530

    removed hmr

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Replace <REPO_NAME> with your repo name
 export default defineConfig({
-  server: {
-    hmr: {
-      overlay: false,
-    },
-  },
+  // server: {
+  //   hmr: {
+  //     overlay: false,
+  //   },
+  // },
   plugins: [
     react(),
     tailwindcss()
